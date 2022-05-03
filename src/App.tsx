0c92720f5e2a972f68c86f8a513439bb77ec739a
Victor Santos <victor.engsantos@gmail.com>
2022-05-03 17:35:19 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <span className="text-blue-400">
+      <a
+        className="underline underline-offset-1"
+        href="https://reactjs.org"
+        target="_blank"
+      >
+        Learn ReactJS
+      </a>
+    </span>
+  )
+}
